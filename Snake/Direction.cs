3f1a2453e06ebcc29a6316786916de9f6d4eda76
Roberto Ramirez <robert.624@hotmail.com>
2023-06-21 11:22:49 -0500

    Agregar archivos de proyecto.

---
+++ Snake/Direction.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Snake
+{
+    public class Direction
+    {
+        public readonly static Direction Left = new Direction(0, -1);
+        public readonly static Direction Right = new Direction(0, 1);
+        public readonly static Direction Up = new Direction(-1, 0);
+        public readonly static Direction Down = new Direction(1, 0);
+
+        public int RowOffset { get; }
+        public int ColumnOffset { get; }
+
+        public Direction() { }
+
+        public Direction(int rowOffset, int columnOffset)
+        {
+            RowOffset = rowOffset;
+            ColumnOffset = columnOffset;
+        }
+
+
+    }
+}

e078fcb2ce716638f6fd4613c4596d7685c339cb
Roberto Ramirez <robert.624@hotmail.com>
2023-06-23 10:59:42 -0500

    Created main UI: window and game grid

--- Snake/MainWindow.xaml.cs
+++ Snake/MainWindow.xaml.cs
@@ -20,9 +20,48 @@ namespace Snake
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly int rows = 15, columns = 15;
+        private readonly Image[,] gridImages;
+
+        private readonly Dictionary<GridValue, ImageSource> gridValToImage = new()
+        {
+            { GridValue.Empty, Images.Empty },
+            { GridValue.Snake, Images.SnakeBody },
+            { GridValue.Food, Images.Food },
+        };
+
         public MainWindow()
         {
             InitializeComponent();
+            gridImages = SetUpGrid();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private Image[,] SetUpGrid()
+        {
+            Image[,] gridImages = new Image[rows, columns];
+            GameGrid.Rows = rows;
+            GameGrid.Columns = columns;
+
+            for(int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < columns; c++)
+                {
+                    Image image = new Image
+                    {
+                        Source = Images.Empty,
+                    };
+
+                    gridImages[r, c] = image;
+                    GameGrid.Children.Add(image);
+                }
+            }
+
+            return gridImages;
         }
     }
 }
